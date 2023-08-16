db0b418e3ae5872aa6363d42029d0d1a2c6144c2
Administrator <1328497946@qq.com>
2023-08-16 16:50:04 +0800

    添加全局路由前缀

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // 全局路由前缀
+  app.setGlobalPrefix('api/v1');
   await app.listen(3000);
 }
 bootstrap();
