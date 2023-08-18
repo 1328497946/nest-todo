6e33dea519b28d606fd7b788dba304eac0fa3a9f
Administrator <1328497946@qq.com>
2023-08-18 10:40:32 +0800

    删除dist目录

--- dist/config/app.config.d.ts
+++
@@ -1,6 +0,0 @@
-declare const _default: () => {
-    port: number;
-    environment: string;
-    database: import("typeorm").DataSourceOptions;
-};
-export default _default;
--- dist/config/app.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const database_config_1 = require("./database.config");
-exports.default = () => ({
-    port: 3000,
-    environment: 'development',
-    database: database_config_1.ormConfig,
-});
-//# sourceMappingURL=app.config.js.map
