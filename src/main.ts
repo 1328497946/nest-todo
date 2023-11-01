bddb501d11a72a4a015e7d4384c612a5374b1e7f
ao <1328497946@qq.com>
2023-11-02 00:24:11 +0800

    :sparkles: feat(Global): 添加全局interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ResponseInterceptor } from './response/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // 配置全局uniform response format
+  app.useGlobalInterceptors(new ResponseInterceptor());
   // 全局路由前缀
   app.setGlobalPrefix('api/v1');
   // 允许跨域
