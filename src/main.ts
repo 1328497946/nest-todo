47c04607ae55647cff9e68c554f57fad6ffec4b7
ao <1328497946@qq.com>
2023-11-02 10:10:30 +0800

    :sparkles: feat(Global): 添加全局response uniform format和全局exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ResponseInterceptor } from './response/response.interceptor';
+import { HttpExceptionFilter } from './execption/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // 配置全局uniform response format
-  app.useGlobalInterceptors(new ResponseInterceptor());
   // 全局路由前缀
   app.setGlobalPrefix('api/v1');
   // 允许跨域
   app.enableCors();
+  // 配置全局uniform response format
+  app.useGlobalInterceptors(new ResponseInterceptor());
+  // 配置全局Exception Filter
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
--- src/response/response.interceptor.ts
+++ src/response/response.interceptor.ts
@@ -1,49 +1,33 @@
 import {
   CallHandler,
   ExecutionContext,
-  HttpException,
-  HttpStatus,
   Injectable,
   NestInterceptor,
 } from '@nestjs/common';
-import { Observable, catchError, map, throwError } from 'rxjs';
+import { Response } from './interface';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable()
-export class ResponseInterceptor implements NestInterceptor {
-  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    return next.handle().pipe(
-      map((res: unknown) => this.responseHandler(res, context)),
-      catchError((err: HttpException) =>
-        throwError(() => this.errorHandler(err, context)),
-      ),
-    );
+export class ResponseInterceptor<T> implements NestInterceptor<T, Response<T>> {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<Response<T>> {
+    return next
+      .handle()
+      .pipe(map((res: T) => this.responseHandler(res, context)));
   }
-  errorHandler(exception: HttpException, context: ExecutionContext) {
-    const ctx = context.switchToHttp();
-    const response = ctx.getResponse();
-    const request = ctx.getRequest();
-    const status =
-      exception instanceof HttpException
-        ? exception.getStatus()
-        : HttpStatus.INTERNAL_SERVER_ERROR;
-    response.status(status).json({
-      success: false,
-      statusCode: status,
-      path: request.url,
-      message: exception.message,
-      result: exception,
-    });
-  }
-  responseHandler(res: any, context: ExecutionContext) {
+  responseHandler(res: T, context: ExecutionContext): Response<T> {
     const ctx = context.switchToHttp();
     const response = ctx.getResponse();
     const request = ctx.getRequest();
     const statusCode = response.statusCode;
     return {
-      succes: true,
+      success: true,
       path: request.url,
-      statusCode,
-      result: res,
+      code: statusCode,
+      data: res,
     };
   }
 }
